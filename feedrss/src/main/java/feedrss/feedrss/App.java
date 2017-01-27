de21ab7af21436c957109227965988e4dbdb73a5
notoriousPig <alexandre.payen@epitech.eu>
2017-01-27 20:20:16 +0100

    update server

--- feedrss/src/main/java/feedrss/feedrss/App.java
+++ feedrss/src/main/java/feedrss/feedrss/App.java
@@ -2,7 +2,9 @@ package feedrss.feedrss;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Enumeration;
 import java.util.Properties;
 
@@ -14,9 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mysql.jdbc.DatabaseMetaData;
 import com.mysql.jdbc.PreparedStatement;
-import com.mysql.jdbc.ResultSet;
 import com.mysql.jdbc.ResultSetMetaData;
-import com.mysql.jdbc.Statement;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -37,7 +37,7 @@ import java.util.Arrays;
 public class App 
 {
 
-	    public static void main(String[] args) throws Exception {
+	/*    public static void main(String[] args) throws Exception {
 	        RssParser parser = new RssParser(
 	                "http://www.bfmtv.com/rss/info/flux-rss/flux-toutes-les-actualites/");
 	read read = parser.readFeed();
@@ -46,62 +46,119 @@ public class App
 	       System.out.println(message);	        
 	    }
 	    }
-	
-    //private static ServerSocket welcomeSocket;
+	 */
+	private static ServerSocket welcomeSocket;
 
-/*	public static void main( String[] args ) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
-    {
-    	
-    	 String clientSentence,clientpassword;
-         String capitalizedSentence;
-        // welcomeSocket = new ServerSocket(4009);
-         Connection con = null;
-         String url = "jdbc:mysql://localhost:3306/";
-         String db = "feedrss";
-         String driver = "com.mysql.jdbc.Driver";
-         String user = "root";
-         String pass = "toto";
-         System.out.println("bordel");
-        /* while(true)
-         {
-        	 System.out.println("bordel2");
-            Socket connectionSocket = welcomeSocket.accept();
-            System.out.println("bordel99");
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            System.out.println("bordel3");
-            BufferedReader inFromClient1 = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            System.out.println("bordel4");
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            System.out.println("bordelde merde ");
-            clientSentence = inFromClient.readLine(); 
-            clientpassword = inFromClient.readLine();
-            
-            System.out.println("Received User Name: " + clientSentence);
-            System.out.println("Received Password: " + clientpassword);
-            Class.forName(driver).newInstance();
-            con = DriverManager.getConnection(url+db, user, pass);
-            Statement st = (Statement) con.createStatement();
-            ResultSet res = (ResultSet) st.executeQuery("SELECT * FROM  people WHERE Loginr='clientSentence' AND Password='clientpassword'");
-            while (res.next()) { 
-            String u = res.getString("user");
-            String p = res.getString("password");
-            if (clientSentence.equals(u) && clientpassword.equals(p)){
-            capitalizedSentence = "Welcome "+clientSentence+" \n";
-            outToClient.writeBytes(capitalizedSentence); 
-           }else{  
-            capitalizedSentence = "Sorry, not authorized \n";
-            outToClient.writeBytes(capitalizedSentence); 
-            con.close();
-         
-         RssParser parser = new RssParser(
-	                "http://www.bfmtv.com/rss/info/flux-rss/flux-toutes-les-actualites/");
-	read read = parser.readFeed();
-	System.out.println(read);
-	for (ReadRss message : read.getMessages()) {
-	       System.out.println(message);
+	public static void getRss(Statement st, String user_id, DataOutputStream outToClient) throws SQLException, IOException
+	{
+		String query = "SELECT * FROM url WHERE user_id='" + user_id + "';";
+		ResultSet res = (ResultSet) st.executeQuery(query);
+		if (res.first())
+		{
+			do {
+				RssParser parser = new RssParser(res.getString("myurl"));
+				read read = parser.readFeed();
+				for (ReadRss message : read.getMessages()) {
+					outToClient.writeBytes(message.toString());
+				}
+			} while (res.next());
+		}
+
 	}
-           }    */
 
-       }
-		
-		
+	public static void main( String[] args ) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
+	{
+
+		String clientSentence,clientpassword;
+		String capitalizedSentence;
+		welcomeSocket = new ServerSocket(4010);
+		Connection con = null;
+		String url = "jdbc:mysql://localhost:3306/";
+		String db = "feedrss";
+		String driver = "com.mysql.jdbc.Driver";
+		String user = "root";
+		String pass = "root";
+		System.out.println("bordel");
+		while(true)
+		{
+			System.out.println("bordel2");
+			Socket connectionSocket = welcomeSocket.accept();
+			System.out.println("bordel99");
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			System.out.println("bordel3");
+			BufferedReader inFromClient1 = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			System.out.println("bordel4");
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			System.out.println("bordelde merde ");
+			clientSentence = inFromClient.readLine(); 
+			clientpassword = inFromClient.readLine();
+
+			System.out.println("Received User Name: " + clientSentence);
+			System.out.println("Received Password: " + clientpassword);
+			Class.forName(driver).newInstance();
+			con = DriverManager.getConnection(url+db, user, pass);
+			Statement st = (Statement) con.createStatement();
+			String query = "SELECT * FROM people WHERE login='" + clientSentence + "' AND password='" + clientSentence + "';";
+			ResultSet res = (ResultSet) st.executeQuery(query);
+			if (res.first())
+			{
+				String u = res.getString("login");
+				String p = res.getString("password");
+				String user_id = res.getString("id");
+				if (clientSentence.equals(u) && clientpassword.equals(p)){
+					capitalizedSentence = "Welcome "+clientSentence+" \n";
+					outToClient.writeBytes(capitalizedSentence);
+					boolean isUp = true;
+					while (isUp) //read cmd here
+					{
+						String cmd;
+						cmd = inFromClient.readLine();
+						System.out.println("Received cmd: " + cmd);
+						if (cmd.compareTo("/rss") == 0)
+						{
+							getRss(st, user_id, outToClient);
+						}
+						else if (cmd.compareTo("/logout") == 0)
+						{
+							capitalizedSentence = "200 - OK\n";
+							outToClient.writeBytes(capitalizedSentence); 							
+							res.close();
+							st.close();
+							con.close();
+							inFromClient.close();
+							outToClient.close();
+							connectionSocket.close();
+							isUp = false;
+						}
+						else
+						{
+							capitalizedSentence = "Bad Request!\n";
+							outToClient.writeBytes(capitalizedSentence); 							
+						}
+					}
+				}else{  
+					capitalizedSentence = "Sorry, not authorized \n";
+					outToClient.writeBytes(capitalizedSentence); 
+					res.close();
+					st.close();
+					con.close();
+					inFromClient.close();
+					outToClient.close();
+					connectionSocket.close();
+				} 
+			}
+			else
+			{
+				capitalizedSentence = "Sorry, not authorized \n";
+				outToClient.writeBytes(capitalizedSentence); 
+				res.close();
+				st.close();
+				con.close();
+				inFromClient.close();
+				outToClient.close();
+				connectionSocket.close();
+			}
+		}
+	}
+}
+
