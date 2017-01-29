5866b8aeecb89b0d0cb366fb0efa206697404d40
Alexis Nasselevitch <alex@gods-macbook.home>
2017-01-29 20:37:05 +0100

    adding webapp sources

---
+++ WebApp/src/MyRSSFeedAggregator/MyRSSFeedLoginHandler.java
@@ -0,0 +1,80 @@
+package MyRSSFeedAggregator;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by alex on 26/01/2017.
+ */
+public class MyRSSFeedLoginHandler extends javax.swing.JFrame {
+
+    // Variable definition
+
+    MyRSSFeedSessionHandler j_session;
+
+    // Login constructor
+
+    public MyRSSFeedLoginHandler(MyRSSFeedSessionHandler session) {
+        this.j_session = session;
+    }
+
+    // Form initialization
+    @SuppressWarnings("unchecked")
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.jLabel5.setText("Connecting session...");
+        this.j_session.setMail(this.jTextField1.getText());
+        this.j_session.setPassword(this.jPasswordField1.getText());
+        int r = this.j_session.Login();
+        if (r == 200)
+        {
+            this.jLabel5.setText("Connected.");
+            this.setVisible(false);
+            try {
+                new MyRSSFeedGUI(j_session).setVisible(true);
+            } catch (IOException | JSONException ex) {
+                Logger.getLogger(MyRSSFeedLoginHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        try {
+            this.jLabel5.setText("Registration in process...");
+            this.j_session.setMail(this.jTextField1.getText());
+            this.j_session.setPassword(this.jPasswordField1.getText());
+
+            int r = this.j_session.Register();
+            if (r == 200)
+            {
+                this.jLabel5.setText("Connected.");
+                this.dispose();
+                try {
+                    new MyRSSFeedGUI(j_session).setVisible(true);
+                } catch (IOException | JSONException ex) {
+                    Logger.getLogger(MyRSSFeedLoginHandler.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } catch (JSONException | IOException ex) {
+            Logger.getLogger(MyRSSFeedLoginHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ WebApp/src/MyRSSFeedAggregator/MyRSSFeedSessionHandler.java
@@ -0,0 +1,172 @@
+package MyRSSFeedAggregator;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.lang.Object;
+
+/**
+ * Created by alex on 26/01/2017.
+ */
+public class MyRSSFeedSessionHandler {
+
+    // Variable definitions
+    private String j_api;
+    private String j_token;
+    private String u_email;
+    private String u_pwd;
+
+    private int response_code;
+    private JSONArray js_response;
+
+    private Boolean debug;
+
+    // Session Constructor ; receiving api url
+
+    public MyRSSFeedSessionHandler(String api_url) throws IOException {
+        this.j_api = api_url;
+        this.j_token = null;
+        this.j_debug = false;
+    }
+
+    // Getters
+
+    public String getMail(){
+        return this.u_email;
+    }
+
+    public String getToken(){
+        return this.j_token;
+    }
+
+    public String getPassword(){
+        return this.u_pwd;
+    }
+
+    public JSONArray getResponse(){
+        return this.js_response;
+    }
+
+    public int getResponseCode(){
+        return this.response_code;
+    }
+
+    // Setters
+
+    public void setMail(String mail){
+        this.u_email = mail;
+    }
+
+    public void setToken(String token){
+        this.j_token = token;
+    }
+
+    public void setPassword(String password){
+        this.u_pwd = password;
+    }
+
+    // Comm function: Sending request
+
+    public void sendRequest(String route, Map<String, String> params, String method, String cookie,
+                            String urldebug) throws JSONException, IOException {
+        if (this.j_debug)
+            this.j_api = urldebug;
+
+        URL url = new URL(this.j_api + route);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+        con.setRequestMethod(method);
+        con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1");
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        con.setRequestProperty("Content-Type","application/json");
+        if (cookie.length() > 0)
+            con.setRequestProperty("Cookie","token="+this.getToken());
+        if (!"GET".equals(method))
+        {
+            JSONObject js_data = new JSONObject();
+
+            for (Map.Entry<String, String> entry : params.entrySet())
+                js_data.put(entry.getKey(), entry.getValue());
+
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.writeBytes(js_data.toString());
+                wr.flush();
+            }
+        }
+        int responseCode = con.getResponseCode();
+        BufferedReader in = new BufferedReader( new InputStreamReader(con.getInputStream()));
+        String output;
+        StringBuffer resp = new StringBuffer();
+        while ((output = in.readLine()) != null) { resp.append(output); }
+
+        String js_data = null;
+        if (resp.toString().length() == 0){
+            this.response_code = responseCode;
+            return;
+        }
+        else if (resp.toString().charAt(0) == '[')
+            js_data = resp.toString();
+        else if (resp.toString().length() > 0)
+            js_data = "["+resp.toString()+"]";
+        this.js_response = new JSONArray(js_data);
+        this.response_code = responseCode;
+    }
+// Registration function: register route
+
+    public int Register() throws JSONException, IOException {
+        Map<String, String> params = new HashMap<>();
+        params.put("mail", this.getMail());
+        params.put("password", this.getPassword());
+
+        Validate.notNull(params, "The email or password cannot be null");
+
+        this.sendRequest("/register", params, "POST", "","http://www.mocky.io/v2/588a188e3000009600fa8b69");
+
+        if (this.getResponse().getJSONObject(0).has("message") && this.getResponseCode() == 201)
+        {
+            this.Login();
+            return this.getResponseCode();
+        }
+        return (-1);
+    }
+
+    // Login function: authentication route
+
+    public int Login() {
+        try {
+            Map<String, String> params = new HashMap<>();
+
+            params.put("mail", this.getMail());
+            params.put("password", this.getPassword());
+
+            Validate.notNull(params, "The email or password cannot be null");
+
+            this.sendRequest("/auth", params, "POST", "","http://www.mocky.io/v2/5883b4490f00008d0b31c043");
+
+            if (this.getResponse().getJSONObject(0).has("token") && this.getResponseCode() == 200)
+            {
+                this.setToken(this.getResponse().getJSONObject(0).getString("token"));
+                return this.getResponseCode();
+            }
+            return this.getResponseCode();
+
+        } catch (ProtocolException | JSONException ex) {
+            Logger.getLogger(RssAggregatorLogin.class.getName()).log(Level.SEVERE, "Protocol Exception", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(RssAggregatorLogin.class.getName()).log(Level.SEVERE, "IOException", ex);
+        }
+        return (-1);
+    }
+}
